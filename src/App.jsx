8eb118fab07b23a6a04d08ad41550cae06668439
Jose <jose.arias.lombardero@gmail.com>
2026-03-02 08:25:41 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import VideoInmersivo from './components/VideoInmersivo';
 import Solucion from './components/Solucion';
 import Arquitectura from './components/Arquitectura';
 import Alineacion from './components/Alineacion';
-import HojaRuta from './components/Hojaruta';
+import HojaRuta from './components/HojaRuta';
 import ArchivosDesclasificados from './components/ArchivosDesclasificados';
 import Laboratorio from './components/Laboratorio';
 import Footer from './components/Footer';
